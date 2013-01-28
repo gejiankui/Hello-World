4c1b9689ddb5c18e43473c36485b94224489ede0
gejiankui <jiankui.ge@gmail.com>
2013-01-28 16:42:45 +0800

    view change
    test change

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
 	public static void main(String []args)
 	{
 		System.out.println("Hello, world! This is my first project using git");
+		System.out.println("need some change");
 	}
 }			
